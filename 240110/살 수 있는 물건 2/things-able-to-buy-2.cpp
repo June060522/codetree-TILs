b1d3aa604e8387bf3246e6707d206611a4458c33
준이 <98876187+June060522@users.noreply.github.com>
2024-01-10 21:22:14 +0900

    Title: 살 수 있는 물건 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240110/살 수 있는 물건 2/things-able-to-buy-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	float a;
+	cin >> a;
+	if (a >= 3000)
+		cout << "book";
+	else if (a >= 1000)
+		cout << "mask";
+        else if (a >= 500)
+		cout << "pen";
+	else
+		cout << "no";
+
+}
