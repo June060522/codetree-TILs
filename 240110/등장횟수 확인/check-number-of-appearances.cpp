85d8f35a83d2d96edc9c601ab28d2ade1c87460f
준이 <98876187+June060522@users.noreply.github.com>
2024-01-10 23:31:38 +0900

    Title: 등장횟수 확인, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240110/등장횟수 확인/check-number-of-appearances.cpp
+++ 240110/등장횟수 확인/check-number-of-appearances.cpp
@@ -6,13 +6,13 @@
 using namespace std;
 
 int main() {
+	int repeat = 0,input;
 	for (int i = 1; i <= 5; i++)
 	{
 		cin >> input;
 		if (input % 2 == 0)
 		{
-			cout << i;
-			return 0;
+			repeat ++;
 		}
-	}
+	}cout << repeat;
 }
