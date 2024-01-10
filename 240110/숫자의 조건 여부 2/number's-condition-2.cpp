b345c629cdbb4aa63ac16e9fbf23975e37b67bea
준이 <98876187+June060522@users.noreply.github.com>
2024-01-10 21:30:01 +0900

    Title: 숫자의 조건 여부 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240110/숫자의 조건 여부 2/number's-condition-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	if (n == 5)
+	{
+		cout << 'A';
+	}
+	else if (n % 2 == 0)
+	{
+		cout << 'B';
+	}
+}
