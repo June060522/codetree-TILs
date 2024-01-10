ba1013507a7f13275a26e2caaf8edd7c5f3f1a63
준이 <98876187+June060522@users.noreply.github.com>
2024-01-10 23:13:18 +0900

    Title: 숫자의 계절은, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240110/숫자의 계절은/season-of-num.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	int a;
+	cin >> a;
+	if (a <= 2 || a == 12)
+		cout << "Winter";
+	else if(a <= 5)
+		cout << "Spring";
+	else if (a <= 8)
+		cout << "Summer";
+	else
+		cout << "Fall";
+}
