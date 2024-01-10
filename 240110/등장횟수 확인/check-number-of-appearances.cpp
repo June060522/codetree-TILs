5b904c784e0a32dd1bd19d014a9a0869292caee1
준이 <98876187+June060522@users.noreply.github.com>
2024-01-10 23:30:12 +0900

    Title: 등장횟수 확인, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240110/등장횟수 확인/check-number-of-appearances.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	for (int i = 1; i <= 5; i++)
+	{
+		cin >> input;
+		if (input % 2 == 0)
+		{
+			cout << i;
+			return 0;
+		}
+	}
+}
