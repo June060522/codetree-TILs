425dc34f363ef8cc889148befe54ddec5071a7e0
준이 <98876187+June060522@users.noreply.github.com>
2024-01-10 20:37:21 +0900

    Title: 비교에 따른 연산, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240110/비교에 따른 연산/operation-based-on-comparison.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	if (a > b)
+		cout << a * b;
+	else
+		cout << b / a;
+
+}
