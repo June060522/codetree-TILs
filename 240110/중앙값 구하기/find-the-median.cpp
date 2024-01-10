3a50a9086ef22398590b717090eda73025a76326
준이 <98876187+June060522@users.noreply.github.com>
2024-01-10 23:20:24 +0900

    Title: 중앙값 구하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240110/중앙값 구하기/find-the-median.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int a, b, c;
+	cin >> a >> b >> c;
+	if (a > b)
+		swap(a, b);
+	if (b > c)
+		swap(b, c);
+	if (a > b)
+		swap(a, b);
+
+	cout << a;
+}
