652a1c8e51ee94ee6600ca710dfc816a01bdc8c6
준이 <98876187+June060522@users.noreply.github.com>
2024-01-10 23:23:55 +0900

    Title: 별 n개 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240110/별 n개 출력하기/print-n-stars.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int i;
+	cin >> i;
+	for (int j = 0; j < i; j++)
+	{
+		cout << "*" << endl;
+	}
+}
