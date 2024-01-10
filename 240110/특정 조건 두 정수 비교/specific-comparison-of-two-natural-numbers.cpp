7f2a6e5dee52725f2161983e28c565b1f0a3faaf
준이 <98876187+June060522@users.noreply.github.com>
2024-01-10 21:36:36 +0900

    Title: 특정 조건 두 정수 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240110/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	if (a < b)
+		cout << 1;
+	else
+		cout << 0;
+	cout << " ";
+	if (a == b)
+		cout << 1;
+	else
+		cout << 0;
+
+}
