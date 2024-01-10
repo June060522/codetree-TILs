31e0dd234b917bd9ab2bce1f882fb314add50cb4
준이 <98876187+June060522@users.noreply.github.com>
2024-01-11 08:11:33 +0900

    Title: 최대공약수 구하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240111/최대공약수 구하기/find-the-greatest-common-divisor.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+int Fun(int a,int b)
+{
+	int c;
+	while (b != 0)
+	{
+		c = a % b;
+		a = b;
+		b = c;
+	}
+	return a;
+}
+
+int Fun1(int a, int b)
+{
+	return (a * b) / Fun(a, b);
+}
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	cout << Fun(a, b);
+}
