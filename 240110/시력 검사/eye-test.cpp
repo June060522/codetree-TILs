d747ad5413e9a8d8ba024b0b5ece2b435aa45a7e
준이 <98876187+June060522@users.noreply.github.com>
2024-01-10 22:03:28 +0900

    Title: 시력 검사, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240110/시력 검사/eye-test.cpp
+++ 240110/시력 검사/eye-test.cpp
@@ -9,7 +9,7 @@ int main() {
 	cin >> f >> f1;
 	if (f >= 1 && f1 >= 1)
 		cout << "High";
-	else if (f >= 1 && f1 >= 1)
+	else if (f >= 0.5f && f1 >= 0.5f)
 		cout << "Middle";
 	else
 		cout << "Low";
