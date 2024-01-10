952c1ff83c5c77c8d2851568164c6b0db7c94a34
준이 <98876187+June060522@users.noreply.github.com>
2024-01-10 23:11:57 +0900

    Title: 굉장한 숫자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240110/굉장한 숫자/amazing-number.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	int a;
+	cin >> a;
+	if ((a % 2 == 1 && a % 3 == 0) || (a % 2 == 0 && a % 5 == 0))
+		cout << "true";
+	else
+		cout << "false";
+}
