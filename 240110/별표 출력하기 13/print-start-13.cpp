dff24900f9d7a01b887673ad3e990bdfdc8ab7e3
준이 <98876187+June060522@users.noreply.github.com>
2024-01-10 21:51:29 +0900

    Title: 별표 출력하기 13, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240110/별표 출력하기 13/print-start-13.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	string s[11];
+	string s1 = "* ";
+	for (int i = 1; i < 11; i++)
+	{
+		s[i] = s1;
+		s1 += "* ";
+	}
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		if (i % 2 == 0)
+			cout << s[n - (i / 2)] << endl;
+		else
+			cout << s[(i / 2) + 1] << endl;
+	}
+
+	for (int i = n - 1; i > 0; i--)
+	{
+		if (i % 2 == 0)
+			cout << s[n - (i / 2)] << endl;
+		else
+			cout << s[(i / 2) + 1] << endl;
+	}
+}
