86a1a8bfcd15a3642366a0928b07a49633aedb10
준이 <98876187+June060522@users.noreply.github.com>
2024-01-12 08:42:17 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240112/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
+++ 240112/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
@@ -8,18 +8,19 @@
 
 using namespace std;
 
-int graph[101][101];
+vector<vector<int>> graph;
 
 int n, m;
 
 void DFS(int x, int y)
 {
-	if (x <= 0 || y <= 0 || x > m || y > n)
+	if (x <= 0 || y <= 0 || x > n || y > m)
 		return;
 
 	if (graph[x][y] != 1)
 		return;
 
+	graph[x][y] = -1;
 	DFS(x + 1, y);
 	DFS(x - 1, y);
 	DFS(x , y + 1);
@@ -28,7 +29,10 @@ void DFS(int x, int y)
 
 int main()
 {
-	int n, m;
+	graph.resize(101);
+	for(int i = 1; i < 101; i++)
+			graph[i].resize(101);
+
 	cin >> n >> m;
 	for (int i = 1; i <= n; i++)
 	{
