d875cc36f60638979d88f4b559325d01a9dfae2e
준이 <98876187+June060522@users.noreply.github.com>
2024-01-11 14:41:39 +0900

    Title: 세 수의 합과 곱, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240111/세 수의 합과 곱/sum-and-product-of-three-numbers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+	cin >> a >> b >> c;
+
+	cout << "sum = " << a + b + c << endl;
+	cout << "mul = " << a * b * c<< endl;
+}
