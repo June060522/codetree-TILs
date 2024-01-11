d99524c00a7794ec4d54295571a36bda3a5f6bfa
준이 <98876187+June060522@users.noreply.github.com>
2024-01-11 14:46:26 +0900

    Title: 연속되는 수 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240111/연속되는 수 2/continuous-number2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+	int arr[1000];
+	int answer = 1, temp = 1;
+	int testcase = 0;
+	cin >> testcase;
+
+	for (int i = 0; i < testcase; i++)
+	{
+		cin >> arr[i];
+	}
+
+	for (int i = 1; i < testcase; i++)
+	{
+		if (arr[i] == arr[i - 1])
+			temp++;
+		else
+		{
+			answer = max(temp, answer);
+			temp = 1;
+		}
+	}
+	answer = max(answer, temp);
+    cout << answer;
+}
