a0690352d7b8704463a692bd686f52412cfad10b
준이 <98876187+June060522@users.noreply.github.com>
2024-01-11 09:37:17 +0900

    Title: 중앙값 계산 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240111/중앙값 계산 2/get-median-2.cpp
+++ 240111/중앙값 계산 2/get-median-2.cpp
@@ -11,13 +11,13 @@ int arr[100];
 int main() {
 	int n;
 	cin >> n;
-	for (int i = 1; i <= n; i++)
+	for (int i = 0; i < n; i++)
 	{
 		cin >> arr[i];
-		if (i % 2 == 1)
+		if (i % 2 == 0)
 		{
-			sort(arr + 1, arr + i);
-			cout << arr[i / 2 + 1] << " ";
+			sort(arr, arr + i + 1);
+			cout << arr[i / 2] << " ";
 		}
 	}
 }
