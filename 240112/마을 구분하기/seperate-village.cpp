cfa7783a91be1d1ca98e4bfa3ff0b734b7932c11
준이 <98876187+June060522@users.noreply.github.com>
2024-01-12 09:02:47 +0900

    Title: 마을 구분하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240112/마을 구분하기/seperate-village.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+vector<vector<int>> graph;
+
+int n, totalcnt = 0;
+priority_queue<int,vector<int>,greater<int>> answerqueue;
+
+int DFS(int x, int y, int num)
+{
+	if (x <= 0 || y <= 0 || x > n || y > n)
+		return 0;
+
+	if (graph[x][y] != num)
+		return 0;
+	graph[x][y] = 0;
+	return 1 + DFS(x + 1, y, num) + DFS(x - 1, y, num) + DFS(x, y - 1, num) + DFS(x, y + 1, num);
+}
+
+int main()
+{
+	graph.resize(101);
+	for (int i = 1; i < 101; i++)
+		graph[i].resize(101);
+
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= n; j++)
+		{
+			cin >> graph[i][j];
+		}
+	}
+
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= n; j++)
+		{
+			if (graph[i][j] != 0)
+			{
+				int x = DFS(i, j, graph[i][j]);
+				totalcnt++;
+				answerqueue.push(x);
+			}
+		}
+	}
+
+	cout << totalcnt << endl;
+	while (!answerqueue.empty())
+	{
+		cout << answerqueue.top() << endl;
+		answerqueue.pop();
+	}
+}
