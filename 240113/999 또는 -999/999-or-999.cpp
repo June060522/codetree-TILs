11c3167d10937dfc2a677532271aaa9f4b61dad9
준이 <98876187+June060522@users.noreply.github.com>
2024-01-13 23:43:42 +0900

    Title: 999 또는 -999, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240113/999 또는 -999/999-or-999.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	int input;
+	int minVal = 1000;
+	int maxVal = -1000;
+	for (int i = 0; i < 100; i++)
+	{
+		cin >> input;
+        if(input == 999 || input == -999)
+        break;
+		minVal = min(minVal, input);
+		maxVal = max(maxVal, input);
+	}
+
+	cout << maxVal << " " << minVal;
+}
