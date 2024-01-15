b6a9c91f45217f7fcbd506d220559c84436af3c3
준이 <98876187+June060522@users.noreply.github.com>
2024-01-15 09:23:40 +0900

    Title: 정사각형 출력, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240115/정사각형 출력/print-square.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	int index = 1;
+	for (int i = 1; i <= n; i++)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			cout << index++ << " ";
+		}
+
+		cout << endl;
+	}
+}
