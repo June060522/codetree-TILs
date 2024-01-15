d21cf78614e53c884bd4ceaa1d9eff953d594c95
준이 <98876187+June060522@users.noreply.github.com>
2024-01-15 09:41:49 +0900

    Title: 차례로 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240115/차례로 출력/print-in-order.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+int main()
+{
+	cout << "5 6 7 8 9 10 11 12 13 14 15 16 17";
+}
