a8acc222062241f7660fd3ca91ecb17780d6b553
준이 <98876187+June060522@users.noreply.github.com>
2024-01-15 09:45:56 +0900

    Title: 증가시키며 출력하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240115/증가시키며 출력하기 2/increase-and-print-2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+int main()
+{
+	cout << "10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26";
+}
