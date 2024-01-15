a441aad936c7c4a0049d5e4ca0904f8de640d5d4
준이 <98876187+June060522@users.noreply.github.com>
2024-01-15 09:37:45 +0900

    Title: 일 수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240115/일 수 구하기/number-of-days-in-month.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+int arr[13] = { 0,31,28,31,30,31,30,31,31,30,31,30,31 };
+int main()
+{
+	int n;
+	cin >> n;
+	cout << arr[n];
+}
