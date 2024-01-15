e6a3f05383fbaac72407d88ded33542fc93869eb
준이 <98876187+June060522@users.noreply.github.com>
2024-01-15 09:49:07 +0900

    Title: 감소시키며 출력하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240115/감소시키며 출력하기 2/decrease-and-print-2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+int main()
+{
+	cout << "26 25 24 23 22 21 20 19 18 17 16 15 14 13 12 11 10";
+}
