6e79f3b1e981b7a1621383cc52b221c01463e7e5
준이 <98876187+June060522@users.noreply.github.com>
2024-01-15 09:50:10 +0900

    Title: n부터 1까지 숫자 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240115/n부터 1까지 숫자 출력/print-n-to-1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = n; i > 0; i --)
+		cout << i << " ";
+}
