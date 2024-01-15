b7675850dcd0987864ea0ad4b45282266a1440c9
준이 <98876187+June060522@users.noreply.github.com>
2024-01-15 09:38:35 +0900

    Title: 최댓값 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240115/최댓값 구하기/maximum-value.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+int main()
+{
+	int a,b,c;
+	cin >> a >> b >> c;
+	cout << max(a,max(b,c));
+}
