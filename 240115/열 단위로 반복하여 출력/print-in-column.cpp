28ac36df0ae01a1205cab7a1c42458f278cce188
준이 <98876187+June060522@users.noreply.github.com>
2024-01-15 09:01:30 +0900

    Title: 열 단위로 반복하여 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240115/열 단위로 반복하여 출력/print-in-column.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= n; j++)
+			cout << i;
+		cout << endl;
+	}
+}
