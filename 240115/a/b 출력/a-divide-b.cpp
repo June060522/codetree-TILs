bd22192c9166cd4c0e039eafa3a036b78d6b8e5d
준이 <98876187+June060522@users.noreply.github.com>
2024-01-15 09:58:31 +0900

    Title: a/b 출력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240115/a/b 출력/a-divide-b.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+int main()
+{
+	cout << fixed;
+	cout.precision(20);
+	int a, b;
+	cin >> a >> b;
+	cout << a / b;
+}
