288c7269e79b5778045d949cf7ab49719f7301b5
준이 <98876187+June060522@users.noreply.github.com>
2024-01-15 10:13:32 +0900

    Title: 숫자의 증감, Time: 1070ms, Memory: 72MB, Status: Time Limit Exceed - CodeTree

---
+++ 240115/숫자의 증감/increasing-and-decreasing-numbers.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+int main()
+{
+	char c;
+	int i;
+	cin >> c >> i;
+
+	if (c == 'A')
+	{
+		for (int j = 1; j <= i; j++)
+		{
+			cout << j << " ";
+		}
+	}
+	else
+		for (int j = i; j >= 1; j++)
+		{
+			cout << j << " ";
+		}
+}
