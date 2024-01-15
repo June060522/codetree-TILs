af1949b4f3b77a083a33c7ccee45b6dc852637a2
준이 <98876187+June060522@users.noreply.github.com>
2024-01-15 10:41:58 +0900

    Title: 가장 오래 걸리는 학생, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240115/가장 오래 걸리는 학생/longest-student.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <tuple>
+using namespace std;
+
+int n, m;
+int graph[101][101];
+bool visited[101];
+int dist[101];
+int main() {
+    int answer = 0;
+    cin >> n >> m;
+    for (int i = 0; i < m; i++)
+    {
+        int x, y, d;
+        cin >> x >> y >> d;
+        graph[x][y] = d;
+        graph[y][x] = d;
+    }
+    for (int i = 1; i <= n; i++)
+        dist[i] = (int)1e6;
+    dist[n] = 0;
+
+    for (int i = 1; i <= n; i++) {
+        int min_index = -1;
+        for (int j = 1; j <= n; j++) {
+            if (visited[j]) continue;
+            if (min_index == -1 || dist[min_index] > dist[j])
+                min_index = j;
+        }
+        visited[min_index] = true;
+
+        for (int j = 1; j <= n; j++) {
+            if (graph[min_index][j] == 0) continue;
+            dist[j] = min(dist[j], dist[min_index] + graph[min_index][j]);
+        }
+    }
+
+    for (int i = 1; i <= n; i++) {
+        
+        answer = max(answer, dist[i]);
+    }
+    cout << answer;
+    return 0;
+}
