fd4ecdd9add1d51432fc586c11dcf184de048433
준이 <98876187+June060522@users.noreply.github.com>
2024-01-15 10:03:15 +0900

    Title: a/b 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240115/a/b 출력/a-divide-b.cpp
+++ 240115/a/b 출력/a-divide-b.cpp
@@ -14,9 +14,14 @@ using namespace std;
 
 int main()
 {
-	cout << fixed;
-	cout.precision(20);
 	int a, b;
 	cin >> a >> b;
-	cout << a / b;
+	cout << a / b << ".";
+	a %= b;
+	for (int i = 0; i < 20; i++)
+	{
+		a *= 10;
+		cout << a / b;
+		a %= b;
+	}
 }
