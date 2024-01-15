fbe8ef9701dc1b37ac291e2dfe9128b69baaae5d
준이 <98876187+June060522@users.noreply.github.com>
2024-01-15 10:51:37 +0900

    Title: 가장 오래 걸리는 학생, Time: 691ms, Memory: 80MB, Status: Memory Limit Exceed - CodeTree

--- 240115/가장 오래 걸리는 학생/longest-student.cpp
+++ 240115/가장 오래 걸리는 학생/longest-student.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <vector>
 #include <tuple>
+#include <map>
 using namespace std;
 
 int n, m;
-int graph[1001][1001];
-bool visited[1001];
-int dist[1001];
+map<pair<int,int>,int> graph;
+bool visited[100001];
+int dist[100001];
 int main() {
     int answer = 0;
     cin >> n >> m;
@@ -14,11 +15,11 @@ int main() {
     {
         int x, y, d;
         cin >> x >> y >> d;
-        graph[x][y] = d;
-        graph[y][x] = d;
+        graph.insert({{y,x},d});
+        graph.insert({{x,y},d});
     }
     for (int i = 1; i <= n; i++)
-        dist[i] = (int)1e6;
+        dist[i] = 1000000000;
     dist[n] = 0;
 
     for (int i = 1; i <= n; i++) {
@@ -31,8 +32,8 @@ int main() {
         visited[min_index] = true;
 
         for (int j = 1; j <= n; j++) {
-            if (graph[min_index][j] == 0) continue;
-            dist[j] = min(dist[j], dist[min_index] + graph[min_index][j]);
+            if (graph[{min_index, j}] == 0) continue;
+            dist[j] = min(dist[j], dist[min_index] + graph[{min_index, j}]);
         }
     }
 
