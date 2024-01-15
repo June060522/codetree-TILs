2bc525be292c96d532503f27032172f8cb41b7e0
준이 <98876187+June060522@users.noreply.github.com>
2024-01-15 09:13:17 +0900

    Title: 상하로 반복하며 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240115/상하로 반복하며 출력/print-in-up-down-reverse.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= n; j++)
+		{
+			if(j% 2 == 1)
+				cout << i;
+			else
+				cout << abs(n - (i-1));
+		}
+		cout << endl;
+	}
+}
