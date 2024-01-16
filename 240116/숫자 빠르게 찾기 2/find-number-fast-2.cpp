b0c0929e27ba2fc930736419e07e8d6d9a245ff6
준이 <98876187+June060522@users.noreply.github.com>
2024-01-16 10:52:30 +0900

    Title: 숫자 빠르게 찾기 2, Time: 269ms, Memory: 5MB, Status: Passed - CodeTree

---
+++ 240116/숫자 빠르게 찾기 2/find-number-fast-2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <climits>
+
+using namespace std;
+
+int main()
+{
+	set<int> s;
+	int testcase = 0;
+	int m,input;
+	cin >> testcase >> m;
+	for (int i = 0; i < testcase; i++)
+	{
+		cin >> input;
+		s.insert(input);
+	}
+
+	for (int i = 0; i < m; i++)
+	{
+		cin >> input;
+		if (s.lower_bound(input) == s.end())
+			cout << -1 << endl;
+		else
+			cout << *s.lower_bound(input) << endl;
+	}
+}
