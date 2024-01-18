e2dd36cc2cad6a85e70ba3e7e2c288fe7d31784a
준이 <98876187+June060522@users.noreply.github.com>
2024-01-18 10:11:47 +0900

    Title: 높은 수에서 낮은 수까지, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240118/높은 수에서 낮은 수까지/from-high-to-low.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <climits>
+
+using namespace std;
+
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	if (a < b)
+		swap(a, b);
+
+	for (int i = a; i <= b; i++)
+		cout << i << " ";
+}
