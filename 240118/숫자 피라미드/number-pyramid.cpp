08281b018120cbb13af3bfc6896073d3e86c8d0a
준이 <98876187+June060522@users.noreply.github.com>
2024-01-18 10:23:16 +0900

    Title: 숫자 피라미드, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240118/숫자 피라미드/number-pyramid.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <climits>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= i; j++)
+			cout << i << " ";
+		cout << endl;
+	}
+}
