18ee151ac1a7998153b23f883ec6fbd02fa7f131
준이 <98876187+June060522@users.noreply.github.com>
2024-07-17 11:11:20 +0900

    Title: 바이러스 검사, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240717/바이러스 검사/virus-detector.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+int main() {
+	int m, o, p;
+	int input;
+	long long answer = 0;
+	vector<int> peoples;
+	cin >> m;
+
+	for (int i = 0; i < m; i++)
+	{
+		cin >> input;
+		peoples.push_back(input);
+	}
+	cin >> o >> p;
+	for (int i = 0; i < m; i++)
+	{
+		answer += 1;
+		peoples[i] -= o;
+		answer += ceil((float)peoples[i] / p);
+	}
+	cout << answer;
+}
