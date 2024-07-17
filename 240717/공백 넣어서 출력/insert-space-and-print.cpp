ce680fef5041b9a0829f5a0da2bf5f1ea117f656
준이 <98876187+June060522@users.noreply.github.com>
2024-07-17 11:54:47 +0900

    Title: 공백 넣어서 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240717/공백 넣어서 출력/insert-space-and-print.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "score          subject          name"<<endl << endl;
+
+cout << "  100             math           sam"<<endl;
+cout<<"   88          science          paul"<<endl;
+cout<<"   96         language          jack"<<endl;
+}
