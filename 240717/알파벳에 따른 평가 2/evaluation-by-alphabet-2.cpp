e6dfa4f7e174eba21b5504b3a6d48bbc0ded10f0
준이 <98876187+June060522@users.noreply.github.com>
2024-07-17 12:58:34 +0900

    Title: 알파벳에 따른 평가 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240717/알파벳에 따른 평가 2/evaluation-by-alphabet-2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	char c;
+	cin >> c;
+	if (c == 'S')
+		cout << "Superior";
+	if (c == 'F')
+		cout << "Fantastic";
+	if (c == 'G')
+		cout << "Good";
+	if (c == 'U')
+		cout << "Usually";
+	if (c == 'E')
+		cout << "Effort";
+}
