8777bf475e4efc9eed3b97b7f2c4920b124e92eb
준이 <98876187+June060522@users.noreply.github.com>
2024-07-17 12:08:15 +0900

    Title: 다섯 실수의 반올림, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240717/다섯 실수의 반올림/rounding-of-five-actual-numbers.cpp
+++ 240717/다섯 실수의 반올림/rounding-of-five-actual-numbers.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
-	cout << fixed;
-	cout.precision(3);
 	float f;
 	for (int i = 0; i < 5; i++)
 	{
 		cin >> f;
-		cout << f << endl;
+		cout << round(f * 1000) / 1000 << endl;
 	}
 }
